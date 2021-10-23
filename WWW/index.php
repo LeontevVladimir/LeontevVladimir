11f14786023ea087799868c1be184638e30f68ef
LeontevVladimir <vova55u@mail.ru>
2021-10-23 20:53:30 +0500

    update index.php

--- WWW/index.php
+++ WWW/index.php
@@ -4,3 +4,8 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
+<ul>
+<li><a href="http://leontev/WWW/lab-1-1.php">Упражнение 1-2</a></li>
+<li><a href="http://leontev/WWW/lab-1-2.php">Упражнение 1-3</a></li>
+<li><a href="http://leontev/WWW/lab-1-3.php">Упражнение 1-4</a></li>
+</ul>

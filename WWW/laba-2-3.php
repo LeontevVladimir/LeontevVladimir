1bb397b5ffc32d7474ff69926803656d9700e7be
LeontevVladimir <vova55u@mail.ru>
2021-11-08 21:49:09 +0500

    upd laba-2-3

--- WWW/laba-2-3.php
+++ WWW/laba-2-3.php
@@ -1,4 +1,23 @@
 <title>Леонтьев Владимир Алексеевич</title>
 </head>
 <?php
+echo "Ассоциативный массив cust: <br>";
+$cust = array("cnum" => "2001", "cname" => "Hoffman","city" => "London", "snum" => "1001");
+print_r($cust);
+echo "<br>";
+echo "Добавление ключа rating: <br>";
+$cust += ["rating" => "100"];
+print_r($cust);
+echo "<br>";
+echo "Сортировка по значениям: <br>";
+arsort($cust);
+print_r($cust);
+echo "<br>";
+echo "Сортировка по ключам: <br>";
+ksort($cust);
+print_r($cust);
+echo "<br>";
+echo "Сортировка с помощью функции sort(): <br>";
+sort($cust);
+print_r($cust);
 ?>

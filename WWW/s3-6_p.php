245a8daf8397df87b26763aeb221108985a262e8
LeontevVladimir <vova55u@mail.ru>
2021-12-15 03:15:02 +0500

    upd s3-6

--- WWW/s3-6_p.php
+++ WWW/s3-6_p.php
@@ -16,9 +16,9 @@ echo ("<P> Результат:<br>". (implode($Z)) . "</textarea>");
 if (isset($_POST["Second"])){
 $text1=$_POST["p2"];
 $arr1 = preg_split("//u",$text1,-1,PREG_SPLIT_NO_EMPTY);
-for ($i=1;$i<count($arr1);$i++){
-    if ($i>0 and $i<count($arr1)-3){
-        $k=$i+2;
+for ($i=0;$i<count($arr1);$i++){
+    if ($i>=0 and $i<count($arr1)-3){
+        $k=$i+3;
             $arr2[$k] = $arr1[$i];
     }
     if ($i>=count($arr1)-3) {

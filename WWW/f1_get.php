bf4400bf91a61c3e50d24d7c6c1c24745b832963
LeontevVladimir <vova55u@mail.ru>
2021-12-20 22:29:09 +0500

    delete

--- WWW/f1_get.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo ("Здравствуйте, " . $_GET["userName"]);
-echo ("<hr>");
-echo ("Значение скрытого поля hideField равно " .
-$_GET["hideField"]);
-?>
--- WWW/f2_post.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo ("Здравствуйте, " . $_POST["userName_post"]);
-echo ("<hr>");
-echo ("Значение скрытого поля hideField_post равно " . $_POST["hideField_post"]);
-?>
